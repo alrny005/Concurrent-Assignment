81f6f3c751ff3b440953df7d0caa942acc32b069
alrny005 <31717454+alrny005@users.noreply.github.com>
2018-06-07 16:45:13 +0930

    Re-added MapDB class to the project.

---
+++ Multiplayer Snake/src/MapDB.java
@@ -0,0 +1,27 @@
+import org.mapdb.*;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentNavigableMap;
+
+public class MapDB {
+    public void map(int UsersToCreate) {
+
+        DB db = DBMaker.newFileDB(new File("userCredentials"))
+                .closeOnJvmShutdown()
+                .encryptionEnable("password")
+                .make();
+
+        // open existing an collection (or create new)
+        ConcurrentNavigableMap<String, String> userCredentials = db.getTreeMap("userCredentials");
+        //userCredentials.put(username password)
+
+        //put 100
+
+        db.commit();
+
+        db.close();
+    }
+
+}
