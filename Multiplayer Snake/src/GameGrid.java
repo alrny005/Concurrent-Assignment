40d366e28cf8a857cf749c575c9a86fa4836ee7e
alrny005 <31717454+alrny005@users.noreply.github.com>
2018-06-08 12:33:43 +0930

    Updated comments.

--- Multiplayer Snake/src/GameGrid.java
+++ Multiplayer Snake/src/GameGrid.java
@@ -2,20 +2,20 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * A GameGrid class that represents the grid on which the game of Snake will be played on.
- * This class has synchronized methods so that it is able to safely communicate (thread-wise) with the Snake threads.
  *
- *@author Norris Alrichani, Liam Clark
+ *@author Norris Alrichani (alrny005), Liam Clark
  *
+ * Norris Alrichani - Refactored the original code to allow the grid to work in a separate class.
  * Liam Clark - converted the integer array grid into a concurrent hashmap that uses the width variable to simulate
- * a 2 Dimensional map
+ *              a 2-Dimensional map.
  *
  */
 
 public class GameGrid {
-    private int width;
-    private ConcurrentHashMap<Integer,Integer> grid;
+    private ConcurrentHashMap<Integer,Integer> grid;    // An object that represents the grid that the game will run on.
+    private int width;                                  // The width of the grid, which can be set to a desired size.
 
-
+    // Syncrhonized as we definitely want only one instance setting the grid at any given time.
     public synchronized void setGrid(ConcurrentHashMap<Integer,Integer> grid, int gridWidth) {
         this.grid = grid;
         width = gridWidth;
@@ -29,8 +29,8 @@ public class GameGrid {
      *      FOOD_MALUS = 2;
      *      BIG_FOOD_BONUS = 3;
      *      SNAKE = 4;
-     * @param i
-     * @param j
+     * @param i The horizontal value of the location being retrieved.
+     * @param j The vertical value of the location being retrieved.
      * @return grid[i][j]
      */
     public int getStatus(int i, int j) {
@@ -39,9 +39,9 @@ public class GameGrid {
 
     /**
      * Set the status (as defined in the Game class) at a given location in the grid.
-     * @param i
-     * @param j
-     * @param status
+     * @param i The horizontal value of the location being retrieved.
+     * @param j The vertical value of the location being retrieved.
+     * @param status The value stored at the x, y location.
      */
     public void setStatus(int i, int j, int status) {
         grid.put(((j*width)+i),status);
